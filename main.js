e62d5aca20508f91471d47705eed5591574d4f20
cbowman422 <chrisb0wman@hotmail.com>
2023-08-02 18:27:31 -0400

    prompt 3/4

--- main.js
+++ main.js
@@ -1,16 +1,53 @@
 
-
 /*
  * Prompt 1:
- *  Set the numberProblem variable to a number data type. Then on the next line use string concatenation to display your variable with a new string. This new string can either be in the console.log() or you can set a new variable to your string concatenation.
+ *
+ * Set the variable simpleMath to a number. In the console.log() multiply that
+ * variable by a new number. Do this for division, addition, and subtraction
+ * as well using seperate lines and console.logs() for each.
  */
 
-let numberProblem;
+// let simpleMath;
 
-/*  Example Answer => 
-
-let numberProblem = 101;
-let stringConcat = "I got " + numberProblem + " problems but this aint one."
-console.log(stringConcat); 
-
+/*  // Example Answer => 
+let simpleMath = 101;
+console.log(simpleMath*100);
 */
+
+
+
+/*
+ * Prompt 2:
+ *
+ * Declare 3 variables using the variable names numberVariable, booleanVariable,
+ * and stringVariable. Set each variable to values with the respective data
+ * type (number, boolean, and string). Use typeof in your console.log() to 
+ * double check your solutions.
+ */
+
+
+
+/*
+ * Prompt 3:
+ *
+ * Take the following given variables, and using string concatenation, return
+ * them as 4 different words.
+ */
+
+// let wordPlayR = "r"
+// let wordPlayE = "e"
+// let wordPlayA = "a"
+// let wordPlayD = "d"
+
+
+
+/*
+ * Prompt 4:
+ *
+ * Set the given variable's value to a number data type. Then on the next line
+ * use string concatenation to display your variable with a new string. This new
+ * string can either be in the console.log() or you can set a new variable to
+ * your string concatenation.
+ */
+
+// let numberString;
