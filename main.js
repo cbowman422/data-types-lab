f602c30b385f4f4e8af5ed46b55baf5b6c7f21cf
cbowman422 <chrisb0wman@hotmail.com>
2023-08-03 11:50:06 -0400

    prompt 9

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
+// Data Types Lab
 
 /*
  * Prompt 1:
  *
- * Set the variable simpleMath to a number. With a new variable or in the 
+ * Set the variable simpleMath to a number. In the 
  * console.log() multiply that variable by a new number. Do this for 
- * division, addition, subtraction and "division remainder" well using 
+ * division, addition, subtraction and "division remainder" while using 
  * seperate lines for each.
  */
 
-// let simpleMath;
+let simpleMath;
 
 /*  // Example Answer => 
 let simpleMath = 101;
@@ -36,10 +37,10 @@ console.log(simpleMath*100);
  * method.
  */
 
-// let valR = "r";
-// let valE = "e";
-// let valA = "a";
-// let valD = "d";
+let valR = "r";
+let valE = "e";
+let valA = "a";
+let valD = "d";
 
 
 
@@ -50,10 +51,10 @@ console.log(simpleMath*100);
  * + operator, return all of the variables together.
  */
 
-// let num0 = 0;
-// let num1 = 1;
-// let num2 = 2;
-// let num3 = 3;
+let num0 = 0;
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
 
 
 
@@ -69,15 +70,14 @@ console.log(simpleMath*100);
  * template literals, which can make your code easier to read.
  */
 
-// let favAdjective;
-// let favFood;
-// let hackerName;
+let favAdjective;
+let favFood;
+let hackerName;
 
 /*  // Example Answer => 
   console.log(hackerName);
   "Hello my hacker name is smooth quesadilla"
 */
-
 
 
 
@@ -95,13 +95,13 @@ console.log(simpleMath*100);
 //  F) A greater than or equal (>=) that is false.
 //  G) A strict unequal (!==) to create a statement that is true.
  
-// let comparison1 = "strict" === "strict";
-// let comparison2;
-// let comparison3;
-// let comparison4;
-// let comparison5;
-// let comparison6;
-// let comparison7;
+let comparison1 = "strict" === "strict";
+let comparison2;
+let comparison3;
+let comparison4;
+let comparison5;
+let comparison6;
+let comparison7;
 
 //console.log(comparison1)
 
@@ -114,32 +114,32 @@ console.log(simpleMath*100);
  * statement evaluates to true. The first one is done for you.
  */
 
-// let a = 7;
-// let b = 9;
-// let c = 11;
-// let d = 13;
-// let e = "fifteen";
+let a = 7;
+let b = 9;
+let c = 11;
+let d = 13;
+let e = "fifteen";
 
-// // b -- a;
-// let operators1 = a == b;
+// b -- a;
+let operators1 = a == b;
 
-// // c -- d;
-// let operators2;
+// c -- d;
+let operators2;
 
-// // "ten" -- "twelve";
-// let operators3;
+// "ten" -- "twelve";
+let operators3;
 
-// // d -- c -- b;
-// let operators4;
+// d -- c -- b;
+let operators4;
 
-// // a -- b -- b;
-// let operators5;
+// a -- b -- b;
+let operators5;
 
-// // e -- "sixteen";
-// let operators6;
+// e -- "sixteen";
+let operators6;
 
-// // 1 -- "one";
-// let operators7;
+// 1 -- "one";
+let operators7;
 
 
 
@@ -157,4 +157,29 @@ let tern4 = 5? 6:7;
 let tern5 = false? 7:9;
 let tern6 = "string" ? 1:8;
 
-console.log(tern1, tern2, tern3, tern4, tern5, tern6)
+
+/*
+ * Prompt 9:
+ *
+ * Retrieve "Nemo" from each of the following arrays.
+ */
+
+const array1 = ["Darla", "Nemo", "Bruce", "Chum"];
+const array2 = [
+  ["Bruce", "Chum", "Darla"],
+  ["Chum", "Nemo", "Bruce"],
+  ["Darla", "Bruce", "Chum"],
+];
+const array3 = [
+  ["Chum", ["Darla", "Bruce"]],
+  ["Darla", ["Bruce", ["Nemo"], "Chum"]],
+];
+const array4 = [
+  ["Bruce", "Darla"],
+  ["Darla", ["Bruce", ["Darla"], "Chum"], [["Bruce"]]],[["Nemo"]],["Darla"],
+];
+
+let retrieve1 = array1[1];
+let retrieve2 = array2;
+let retrieve3 = array3;
+let retrieve4 = array4;
